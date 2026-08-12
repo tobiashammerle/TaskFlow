55738d9175c7b89d0c445597639ee6d5da89f018
Tobias Hammerle <Tobias.Hammerle@gmx.de>
2026-08-12 08:55:52 +0200

    test: add end-to-end test for removing task through CLI

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -49,4 +49,23 @@ def test_user_can_complete_task_through_cli(tmp_path: Path, monkeypatch, capsys)
     assert 'Aufgabe "Python lernen" wurde als erledigt markiert' in captured.out
     tasks = repository.get_all()
     assert tasks[0].completed is True
-    
+
+def test_user_can_remove_task_through_cli(tmp_path: Path, monkeypatch, capsys) -> None:
+    database_path = tmp_path /"test_tasks.db"
+    repository = SqliteTaskRepository(database_path)
+    repository.initialize_database()
+    task_service = TaskService(repository)
+    inputs = iter(["1", "Python lernen", "4", "1", "2", "5"])
+    monkeypatch.setattr(
+        "builtins.input",
+        lambda _: next(inputs),
+    )
+    run_cli(task_service)
+    captured = capsys.readouterr()
+    assert "Aufgabe wurde hinzugefügt." in captured.out
+    assert "Python lernen" in captured.out
+    assert 'Die Aufgabe "Python lernen" wurde gelöscht.' in captured.out
+    tasks = repository.get_all()
+    assert len(tasks) == 0
+
+
